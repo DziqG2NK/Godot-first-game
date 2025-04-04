dab4da69df4474d09f2a801cea33f3eda49408d2
Marcin Walendzik <mracin.walendzik.dysk@gmail.com>
2025-04-05 01:32:20 +0200

    Moving foxy

--- player/foxy.gd
+++ player/foxy.gd
@@ -2,9 +2,9 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0
 
-@onready var anim = get_node("AnimatedSprite2D")
+@onready var anim = get_node("AnimationPlayer")
 
 func _ready() -> void:
 	anim.play("Idle")
@@ -22,11 +22,22 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction == -1:
+		get_node("AnimatedSprite2D").flip_h = true
+	
+	elif direction == 1:
+		get_node("AnimatedSprite2D").flip_h = false
+	
 	if direction:
 		velocity.x = direction * SPEED
-		anim.play("Run")
+		if velocity.y == 0:
+			anim.play("Run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		anim.play("Idle")
+		if velocity.y == 0:
+			anim.play("Idle")
+
+	if velocity.y > 0:
+		anim.play("Fall")
 
 	move_and_slide()
